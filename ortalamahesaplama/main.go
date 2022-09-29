d0f33fef3fe38316ed49067dec06e1495220a5dc
scholak <scholaksebahattin@gmail.com>
2022-09-29 23:27:31 +0300

    soru çözüldü

--- ortalamahesaplama/main.go
+++ ortalamahesaplama/main.go
@@ -10,6 +10,38 @@ package ortalamahesaplama
 // OrtalamaHesapla fonksiyonu `go test ./...` komutu ile test edilmelidir
 // Testlerin hepsi PASS olmalıdır
 
-func OrtalamaHesapla(vizeNot, finalNot int, insiyatif float64) float64 {
-	return 0
+func OrtalamaHesapla(vizeNot int, finalNot int, insiyatif float64) float64 {
+	ortalama := (float64(vizeNot) * 0.4) + (float64(finalNot) * 0.6)
+
+	if ortalama < 65 && ortalama * (1 + insiyatif) > 65 {
+		ortalama = 65
+	}
+
+	return ortalama
 }
+
+/*
+
+Aşağıdaki örnekte vize, final, ve insiyatif notlarının minimum ve maksimum değerleri de kontrol ediliyor. Üstte verilen  koşula uymadıkları taktirde sistem hata verir.
+
+func OrtalamaHesapla(vizeNot int, finalNot int, insiyatif float64) (float64 error) {
+	ortalama := (float64(vizeNot) * 0.4) + (float64(finalNot) * 0.6)
+	if vizeNot < 0 || vizeNot > 100 {
+        return errors.New("Geçersiz vize notu!")
+    }
+
+	if finalNot < 0 || finalNot > 100 {
+        return errors.New("Geçersiz final notu!")
+    }
+
+	if insiyatif < 0 || insiyatif > 1 {
+        return errors.New("Geçersiz insiyatif!")
+    }
+
+	if ortalama < 65 && ortalama * (1 + insiyatif) > 65 {
+		ortalama = 65
+	}
+
+	return ortalama
+}
+*/
